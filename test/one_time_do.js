260a2e7bda0a94dffc685aa93aca9b725df64c3a
youqingkui <youqingkui@qq.com>
2015-10-07 18:08:34 +0800

    增加收藏完后更新

--- test/one_time_do.js
+++ test/one_time_do.js
@@ -57,7 +57,36 @@
         p = new PushEvernote(item.url, noteStore, item.noteBook);
         return p.pushNote(callback);
       }, function() {
-        return console.log("#  all do #");
+        console.log("#  all do #");
+        return async.waterfall([
+          function(cb) {
+            return Task.find({
+              status: 3
+            }, null, {
+              sort: {
+                _id: -1
+              }
+            }, function(err, rows) {
+              if (err) {
+                return txErr({
+                  err: err,
+                  fun: 'TaskFind'
+                }, callback);
+              }
+              return cb(null, rows);
+            });
+          }, function(rows) {
+            return async.eachSeries(rows, function(item, callback) {
+              var u;
+              if (item.guid) {
+                u = new UpdateEvernote(item.url, noteStore, item.noteBook, item.guid, item);
+                return u.upNote(callback);
+              }
+            }, function() {
+              return console.log("# all do up #");
+            });
+          }
+        ]);
       });
     }
   ]);
